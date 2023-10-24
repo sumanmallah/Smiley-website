73d438903e41694e3148c1d65d1885f2adb6afde
Ethan Hindmarsh <ethan@ethan.lol>
2023-10-24 11:31:54 -0500

    add header lol

--- smiley-frontend/src/App.js
+++ smiley-frontend/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Smiley Website :)</h1>
+      <ul className="App-header-nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#test">test test</a></li>
+        <li><a href="#survey" className="App-header-nav-survey-element">Survey</a></li>
+      </ul>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
