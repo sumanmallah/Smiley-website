16f01aa0921180ac8f6b6cccd623d03a3efa5a87
Ethan Hindmarsh <ethan@ethan.lol>
2023-10-26 10:46:44 -0500

    add example form

--- smiley-frontend/src/routes/survey.js
+++ smiley-frontend/src/routes/survey.js
@@ -5,6 +5,13 @@ function Survey() {
     <div className="survey-container">
       <h2>How are you feeling?</h2>
       {/* Survey form content */}
+	  {/* example from w3schools*/}
+	  <form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
     </div>
   );
 }
