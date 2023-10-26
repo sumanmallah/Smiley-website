ecd615a7010ef68371bc7129c2efe036833bb2a6
Ethan Hindmarsh <ethan@ethan.lol>
2023-10-26 10:47:43 -0500

    bugfix: example form

--- smiley-frontend/src/routes/survey.js
+++ smiley-frontend/src/routes/survey.js
@@ -7,11 +7,11 @@ function Survey() {
       {/* Survey form content */}
 	  {/* example from w3schools*/}
 	  <form>
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname">
-</form>
+  <label for="fname">First name:</label><br />
+  <input type="text" id="fname" name="fname" /><br />
+  <label for="lname">Last name:</label><br />
+  <input type="text" id="lname" name="lname" />
+  </form>
     </div>
   );
 }
