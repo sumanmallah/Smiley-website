a142850a4224fd799a6bb92e5eaf041737295711
dke29 <dke29@txstate.edu>
2023-11-01 18:15:55 -0500

    added toggle button

--- smiley-frontend/src/routes/toggle.js
+++ smiley-frontend/src/routes/toggle.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
+import '../css/toggle.css';
+
 function Toggle() {
-    // Survey form code goes here
-  
-    const [currentColor, setCurrentColor] = useState("red");
-  
-    const handleToggleButtonClick = () => {
-      setCurrentColor(currentColor === "red" ? "green" : "red");
-    };
-  
-    return (
-      <div className="color_change">
-        <label htmlFor="toggle-button">Toggle Color</label>
-        <input type="checkbox" id="toggle-button" onClick={handleToggleButtonClick} />
-        <span>Current color: {currentColor}</span>
-      </div>
-    );
-  }
-  
-  export default Toggle;
+  const [currentColor, setCurrentColor] = useState('red');
+  const [isColorBlindMode, setColorBlindMode] = useState(false);
+
+  const handleToggleButtonClick = () => {
+    setCurrentColor(currentColor === 'red' ? 'green' : 'red')};
+
+  const toggleColorMode = () => {
+    setColorBlindMode(!isColorBlindMode);
+  };
+
+  return (
+    <div className={`color_change ${isColorBlindMode ? 'color-blind' : ''}`}>
+      <label htmlFor="toggle-button">Toggle Color</label>
+      <input type="checkbox" id="toggle-button" onClick={handleToggleButtonClick} />
+      <span>Current color: {currentColor}</span>
+      <button onClick={toggleColorMode}>
+        {isColorBlindMode ? 'Color Blind Mode' : 'Normal Mode'}
+      </button>
+    </div>
+  );
+}
+
+export default Toggle;
