85020afbb87ddecea911945b4524eb139766b74e
Nilu Sah <zys5@txstate.edu>
2023-11-01 16:55:26 -0500

    Made surveyform

--- smiley-frontend/src/routes/survey.js
+++ smiley-frontend/src/routes/survey.js
@@ -1,17 +1,49 @@
+import React, { useState } from 'react';
+import '../css/survey.css';
 function Survey() {
-  // Survey form code goes here
+  const [selectedFeeling, setSelectedFeeling] = useState('');
+  const feelings = ['Happy', 'Sad', 'Worried', 'Feeling Low', 'Excited'];
+
+  const handleFeelingChange = (event) => {
+    setSelectedFeeling(event.target.value);
+  };
 
   return (
-    <div className="survey-container">
-      <h2>How are you feeling?</h2>
-      {/* Survey form content */}
-	  {/* example from w3schools*/}
-	  <form>
-  <label for="fname">First name:</label><br />
-  <input type="text" id="fname" name="fname" /><br />
-  <label for="lname">Last name:</label><br />
-  <input type="text" id="lname" name="lname" />
-  </form>
+    <div className="survey">
+      <h2>How are you feeling today?</h2>
+      <form id="survey-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="age">Age:</label>
+          <input type="number" id="age" name="age" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="feeling">How are you feeling today?</label>
+          <select id="feeling" name="feeling" value={selectedFeeling} onChange={handleFeelingChange}>
+            {feelings.map((feeling, index) => (
+              <option key={index} value={feeling}>{feeling}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="comments">Additional Comments:</label>
+          <textarea id="comments" name="comments" rows="4" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>You selected: {selectedFeeling}</p>
     </div>
   );
 }
