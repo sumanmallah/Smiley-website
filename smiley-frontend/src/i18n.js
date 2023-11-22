ff5f324e652c20ebec8bf4e71fa60dda744d1776
dke29 <dke29@txstate.edu>
2023-11-21 18:39:02 -0600

    translation-espaneol

--- smiley-frontend/src/i18n.js
+++ smiley-frontend/src/i18n.js
@@ -12,7 +12,7 @@ const resources = {
       nav_survey_ethan1_button: 'EthanCameraSurvey',
       nav_survey_ethan2_button: 'EthanMicrophoneSurvey',
       page_not_found_message: 'Error 404: Page Not Found',
-
+      
       choose: "Choose...",
       language: "Language: ",
       confirm: "Confirm",
@@ -24,7 +24,16 @@ const resources = {
       youSelected: "You selected",
       survey_comments: "Additional Comments:",
       welcome_text: "Welcome to the Smiley website, your trusted companion in navigating the complexities of emotions and enhancing your emotional well-being. Our mission is to assist users in embracing their emotional diversity and motivating them to maintain a positive outlook throughout the day.",
+      AU:"About Us",
+      OP:"Our Purpose",
+      PT:"Life is a journey filled with an array of emotions, and it's completely normal to experience a wide range of feelings daily. However, there are times when negative emotions can become overwhelming, or motivation seems elusive. At Smiley, we understand the importance of emotional well-being and the positive impact it can have on your life. Our purpose is to provide personalized solutions and resources to help you manage your emotions, boost your motivation, and ultimately lead a more fulfilling and positive life. We believe that every emotion,even the challenging ones, has value and can be a stepping stone towards personal growth.",
+      WWO: "What We Offer",
+      EG: "Emotional Guidance:",
+      Smiley_offers: "Smiley offers a range of tools and resources to help you better understand and manage your emotions. Whether you're feeling happy, sad, worried, or anything in between, we're here to guide you.",
+      DM: "Daily Motivation:",
+      MOTIVATION:"We provide daily motivational content to inspire and uplift your spirits. Our goal is to help you maintain a positive mindset and stay motivated throughout the day.",
       
+
     }
   },
   es: {
@@ -47,10 +56,18 @@ const resources = {
       english: "Inglés",
       spanish: "Español",
       french: "Francés",
-      showTheDialog: "Settings but in spanish",
+      showTheDialog: "ajuste",
       youSelected: "Has seleccionado",
-      welcome_text: "Bienvenido al sitio web de Smiley, su compañero de confianza para navegar por las complejidades de las emociones y mejorar su bienestar emocional. Nuestra misión es ayudar a los usuarios a abrazar su diversidad emocional y motivarlos a mantener una perspectiva positiva a lo largo del día."
-    }
+      AU:"Sobre nosotros",
+      OP:"Nuestro propósito",
+      welcome_text: "Bienvenido al sitio web de Smiley, su compañero de confianza para navegar por las complejidades de las emociones y mejorar su bienestar emocional. Nuestra misión es ayudar a los usuarios a abrazar su diversidad emocional y motivarlos a mantener una perspectiva positiva a lo largo del día.",
+      PT:" La vida es una jornada repleta de una variedad de emociones, y es completamente normal experimentar una amplia gama de sentimientos diariamente. Sin embargo, hay momentos en que las emociones negativas pueden se tornar opresivas o una motivación que parece indescriptible. Na Smiley, entendemos la importancia del bienestar emocional y el impacto positivo que puede tener en su vida. Nuestro objetivo es buscar soluciones y recursos personalizados para ayudar a administrar sus emociones, aumentar su motivación y, finalmente, llevar una vida más plena y positiva. Acreditamos que toda la emoción, también como desafiadoras, tem valor e pode ser um trampolim para o crescimento pessoal.",
+      WWO: "Lo que ofrecemos",
+      EG: "Orientación emocional:",
+      Smiley_offers: "Smiley ofrece una gama de herramientas y recursos para ayudarlo a comprender y manejar mejor sus emociones. Ya sea que se sienta feliz, triste, preocupado o cualquier cosa intermedia, estamos aquí para guiarlo.",
+      DM: "Motivación diaria:",
+      MOTIVATION: "Brindamos contenido motivacional diario para inspirarlo y elevar su espíritu. Nuestro objetivo es ayudarlo a mantener una mentalidad positiva y mantenerse motivado a lo largo del día."
+     }
   }
 };
 
--- smiley-frontend/src/routes/about.js
+++ smiley-frontend/src/routes/about.js
@@ -5,25 +5,18 @@ function About() {
   const { t } = useTranslation();
   return (
     <div className="about-container">
-      <h1 id="about-header" className="about-header">About Us</h1>
+      <h1 id="about-header" className="about-header">{t("AU")}</h1>
 
       <p id="welcome-text">{t("welcome_text")}</p>
 
-      <h2 id="purpose-header">Our Purpose</h2>
-      <p id="purpose-text">
-        Life is a journey filled with an array of emotions, and it's completely normal to experience a wide range of feelings daily. However, there are times when negative emotions can become overwhelming, or motivation seems elusive. At Smiley, we understand the importance of emotional well-being and the positive impact it can have on your life.
-        Our purpose is to provide personalized solutions and resources to help you manage your emotions,
-        boost your motivation, and ultimately lead a more fulfilling and positive life. We believe that every emotion,
-        even the challenging ones, has value and can be a stepping stone towards personal growth.
-      </p>
+      <h2 id="purpose-header">{t("OP")}</h2>
+      <p id="purpose-text">{t("PT")}</p>
       <img className="image1" alt="a pic" src="https://miro.medium.com/v2/resize:fit:490/1*lTdwhTFJppOBiA6V-Ygyqg.png" />
       <img className="small-image" alt="a yoga pic" src="https://caffeyoga.com/wp-content/uploads/2016/01/beach-yoga1.jpg" />
-      <h3 id="offer-header">What We Offer</h3>
+      <h3 id="offer-header">{t("WWO")}</h3>
       <p id="offer-text">
-        <strong>Emotional Guidance:</strong> Smiley offers a range of tools and resources to help you better understand and manage your emotions. Whether you're feeling happy, sad, worried, or anything in between, we're here to guide you.
-        <br />
-        <strong>Daily Motivation:</strong> We provide daily motivational content to inspire and uplift your spirits. Our goal is to help you maintain a positive mindset and stay motivated throughout the day.
-        <br />
+        <strong>{t("EG")}</strong> {t("Smiley_offers")}<br/>
+        <strong>{t("DM")}</strong>{t("MOTIVATION")}<br/>
         <strong>Personalized Resources:</strong> We tailor our recommendations to your unique emotional state. Whether you need self-care tips, relaxation techniques, or a boost in confidence, Smiley has you covered.
         <br />
         <strong>Community Support:</strong> Connect with a community of like-minded individuals who are on a journey to enhance their emotional well-being. Share your experiences, gain insights, and provide support to others.
